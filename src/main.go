40e3f401c497d6785b5a2804ae2f58870c6d79a9
e215721 <luongbao124@gmail.com>
2023-02-06 00:33:30 +0900

    last commit

---
+++ src/main.go
@@ -0,0 +1,9 @@
+ package main
+    import (
+        "fmt"
+        "gitlab.ie.u-ryukyu.ac.jp/os/2022/e215721-filewrite/filewrite"
+    )
+    func main() {
+            fmt.Println( filewrite.Hello("Luong Bao"))
+    }
+
